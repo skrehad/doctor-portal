a69ffe2527f68ac4e800237454a1ff93ebb0161c
rehadhasan <rehadhasan664@gmail.com>
2023-05-30 21:56:18 +0600

    update routes

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
-import Main from "../Layout/Main";
-import Home from "../Pages/Home/Home";
-import Login from "../Pages/Login/Login";
+import Main from "../../Layout/Main";
+import Appointment from "../../Pages/Appointment/Appointment/Appointment";
+import Dashboard from "../../Pages/Dashboard/Dashboard/Dashboard";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import SignUp from "../../Pages/SignUp/SignUp";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
-export const router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
@@ -16,6 +20,24 @@ export const router = createBrowserRouter([
         path: "/login",
         element: <Login></Login>,
       },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>,
+      },
+      {
+        path: "/appointment",
+        element: <Appointment></Appointment>,
+      },
     ],
   },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <Dashboard></Dashboard>
+      </PrivateRoute>
+    ),
+  },
 ]);
+
+export default router;
