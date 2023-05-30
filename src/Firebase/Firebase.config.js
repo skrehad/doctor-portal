95e3e4419284e85ff7afe484acbe6149859899be
rehadhasan <rehadhasan664@gmail.com>
2023-05-30 22:07:10 +0600

    update firebase.config

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvyZmoX5k6lLsXBRr_qJgrcwIqqhjr3j4",
+  authDomain: "doctors-portal-a9d65.firebaseapp.com",
+  projectId: "doctors-portal-a9d65",
+  storageBucket: "doctors-portal-a9d65.appspot.com",
+  messagingSenderId: "36499139465",
+  appId: "1:36499139465:web:bedab9317ccd308e43cf58",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
