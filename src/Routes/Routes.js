e8dcfeb4ac21d59f9c0c358ef89c358a688de520
rehadhasan <rehadhasan664@gmail.com>
2023-05-31 10:54:44 +0600

    update home section

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,9 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
-import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
-// import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import Home from "../Pages/Home/Home/Home";
 
 const router = createBrowserRouter([
   {
