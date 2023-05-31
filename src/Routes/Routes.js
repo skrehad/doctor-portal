b7db82c36abe6ec7b08eeac6ce8ff5a870bc8b73
rehadhasan <rehadhasan664@gmail.com>
2023-05-31 14:10:10 +0600

    added Appointment section

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,8 +3,6 @@ import Main from "../Layout/Main";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
 import Home from "../Pages/Home/Home/Home";
-import MakeAppointment from "../Pages/Home/MakeAppointment/MakeAppointment";
-import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
   {
@@ -14,14 +12,6 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
-      },
-      {
-        path: "/appointment",
-        element: (
-          <PrivateRoute>
-            <MakeAppointment></MakeAppointment>
-          </PrivateRoute>
-        ),
       },
       {
         path: "/login",
