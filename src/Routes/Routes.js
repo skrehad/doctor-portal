1cd86ef83b50ed00fde7d505108beee804da8689
rehadhasan <rehadhasan664@gmail.com>
2023-05-31 12:09:41 +0600

    update Routes section

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,6 +3,8 @@ import Main from "../Layout/Main";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
 import Home from "../Pages/Home/Home/Home";
+import MakeAppointment from "../Pages/Home/MakeAppointment/MakeAppointment";
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/appointment",
+        element: (
+          <PrivateRoute>
+            <MakeAppointment></MakeAppointment>
+          </PrivateRoute>
+        ),
       },
       {
         path: "/login",
