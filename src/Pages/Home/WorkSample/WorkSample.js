df2a7fd4c6c0d7622f220e017a3d6d3233eaa72a
rehadhasan <rehadhasan664@gmail.com>
2023-06-01 11:58:42 +0600

    update WorkSample section

--- src/Pages/Home/WorkSample/WorkSample.js
+++ src/Pages/Home/WorkSample/WorkSample.js
@@ -1,7 +1,31 @@
 import React from "react";
+import image from "../../../assets/images/treatment.png";
+import PrimaryButton from "../../../components/PrimaryButton/PrimaryButton";
 
 const WorkSample = () => {
-  return <div>WorkSample</div>;
+  return (
+    <div className="my-16">
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img src={image} alt="" className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">
+              Exceptional Dental Care, on Your Terms
+            </h1>
+            <p className="py-6">
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsumis that it has a more-or-less normal
+              distribution of letters,as opposed to using 'Content here, content
+              here', making it look like readable English. Many desktop
+              publishing packages and web page
+            </p>
+            <PrimaryButton>GET STARTED</PrimaryButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WorkSample;
